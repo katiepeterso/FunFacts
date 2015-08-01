3e471f8f5cdca8e84352947737e7ad262b05ffa7
katiepeterso <katiepeterso@outlook.com>
2015-08-01 17:40:38 -0600

    Initial Commit

---
+++ FunFacts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FunFacts
+//
+//  Created by Katherine Peterson on 2015-08-01.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
